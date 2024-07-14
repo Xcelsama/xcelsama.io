a0b0c32b22a422de2c4e45f8236eb80dddc10b70
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-07-14 10:54:07 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,19 @@
-export default defineAppConfig({
-  // Configure Content Wind here
-  cover: '/cover.jpg',
-  socials: {
-    twitter: 'https://x.com/Xcelsama_',
-    github: 'https://github.com/Xcelsama',
-    linktr.ee: 'https://linktr.ee/xcelsama'
+
+export default {
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  content: {
+    // Content module configuration
+  },
+  tailwindcss: {
+    // TailwindCSS configuration
+  },
+  // Additional configurations
+  appConfig: {
+    cover: '/cover.jpg',
+    socials: {
+      twitter: 'https://x.com/Xcelsama_',
+      github: 'https://github.com/Xcelsama',
+      linktr.ee: 'https://linktr.ee/xcelsama'
+    }
   }
-})
+}
