bb462ba4215ae31ef2b013d2e8c1a843dc53de29
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-07-14 10:57:52 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -11,9 +11,9 @@ export default {
   appConfig: {
     cover: '/cover.jpg',
     socials: {
-      twitter: 'https://x.com/Xcelsama_',
-      github: 'https://github.com/Xcelsama',
-      linktr.ee: 'https://linktr.ee/xcelsama'
+      twitter: 'Xcelsama_',
+      github: 'Xcelsama',
+      linktr.ee: 'xcelsama'
     }
   }
 }
