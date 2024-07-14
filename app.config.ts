79d869e62dc0af119c6cf1148b176b1ddf3e76af
Excel Hephzibah Amadi <ms.excel.amadi@gmail.com>
2024-07-14 10:28:25 +0100

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
   socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind',
+    twitter: 'https://x.com/Xcelsama_',
+    github: 'https://github.com/Xcelsama',
     mastodon: '@atinux@webtoo.ls'
   }
 })
